562191541c345ab2601f96d2970a9a9923c34bab
linyoumu <848516118@qq.com>
2017-12-05 16:35:34 +0800

    init

---
+++ Example/Pods/Target Support Files/LYMCategoryKit/LYMCategoryKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LYMCategoryKit : NSObject
+@end
+@implementation PodsDummy_LYMCategoryKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-LYMCategoryKit_Tests/Pods-LYMCategoryKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LYMCategoryKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LYMCategoryKit_Tests
+@end
